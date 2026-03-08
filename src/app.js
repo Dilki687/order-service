064bf25e93c386d7e417573e16aea54641ab9de2
Dilki687 <chamodidilki44@gmail.com>
2026-03-08 13:27:54 +0530

    add a log

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ app.use("/orders", routes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const PORT = process.env.PORT || 5000;
+console.log("BASE_URL:", process.env.BASE_URL);
 
 app.listen(PORT, () => {
   console.log(`Order Service running on port ${PORT}`);
